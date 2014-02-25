98edf5fe7c3e1f3f9960f4ad9ccdc19701c60a24
Daisuke Takahashi <galaxy.s.two.wimax@gmail.com>
2014-02-25 14:41:50 +0900

    List_Table Upgrade
    * New Navigations
    * New Columns
    * Sort options

--- class.script-list-table.php
+++ class.script-list-table.php
@@ -13,23 +13,43 @@ class OPcache_List_Table extends WP_List_Table {
 		));
 	}
 
+	function extra_tablenav($which) {
+		switch(which) {
+			case 'top':
+				echo 'Extra Table Navigation(Top)';
+				break;
+			case 'bottom':
+				echo 'Extra Table Navigation(Bottom)';
+				break;
+		}
+	}
+
 	function get_columns() {
 		$columns = array(
 			'cb'			=> '<input type="checkbox" />',
 			'script_path'		=> __('Script Path', 'opcache'),
 			'hits'			=> _x('Hits', 'Number of hits', 'opcache'),
 			'memory_consumption'	=> __('Memory Consumption', 'opcache'),
-			'invalidate'		=> __('Invalidate', 'opcache')
+			'last_used_timestamp'	=> __('Last Used', 'opcache'),
+			'timestamp'		=> __('Timestamp', 'opcache'),
 		);
 		return $columns;
 	}
 
+	function get_sortable_columns() {
+		$sortable_columns = array(
+			'script_path'		=> array('full_path', false),
+			'hits'			=> array('hits', false),
+			'memory_consumption'	=> array('memory_consumption', false),
+			'last_used_timestamp'	=> array('last_used_timestamp', false),
+			'timestamp'		=> array('timestamp', false)
+		);
+		return $sortable_columns;
+	}
+
 	function column_cb($item){
 		return sprintf(
-			'<input type="checkbox" name="%1$s[]" value="%2$s" />',
-			/*$1%s*/ $this->_args['singular'],	//Let's simply repurpose the table's singular label ("movie")
-			/*$2%s*/ $item['full_path']		//The value of the checkbox should be the record's id
-		);
+			'<input type="checkbox" name="%1$s[]" value="%2$s" />', $this->_args['singular'], $item['full_path']);
 	}
 
 	function column_script_path($item){
@@ -42,14 +62,11 @@ class OPcache_List_Table extends WP_List_Table {
 
 	function column_default($item, $column_name){
 		switch($column_name) {
-			case "invalidate":
-				return get_submit_button(
-					__('Invalidate', 'opcache'),
-					'primary delete',
-					'invalidate['.$item['full_path'].']',
-					false,
-					array('id' => '')
-				);
+			case 'last_used_timestamp':
+			case 'timestamp':
+				return date(__('j M, Y @ G:i:s', 'opcache'), $item[$column_name]);
+			case 'invalidate':
+				return print_r($item, true);
 			default:
 				return $item[$column_name];
 		}
@@ -64,21 +81,33 @@ class OPcache_List_Table extends WP_List_Table {
 
 	function process_bulk_action() {
 		if('invalidate'===$this->current_action()) {
-			wp_die('Items deleted (or they would be if we had items to delete)!');
+			if(!isset($_GET['script']) or is_array($_GET['script']))
+				return false;
+
+			foreach($_GET['script'] as $script)
+				opcache_invalidate($script);
 		}
 	}
 
 	function prepare_items() {
-		$per_page = 15;
+		$per_page = 20;
 
 		$columns = $this->get_columns();
 		$hidden = array();
-		// $sortable = $this->get_sortable_columns();
-		$sortable = array();
+		$sortable = $this->get_sortable_columns();
 
 		$this->_column_headers = array($columns, $hidden, $sortable);
 
-		// $this->process_bulk_action();
+		$this->process_bulk_action();
+
+		function usort_reorder($a,$b){
+			$orderby = (!empty($_REQUEST['orderby'])) ? $_REQUEST['orderby'] : 'full_path'; //If no sort, default to title
+			$order = (!empty($_REQUEST['order'])) ? $_REQUEST['order'] : 'asc'; //If no order, default to asc
+			$result = strcmp($a[$orderby], $b[$orderby]); //Determine sort order
+			return ($order==='asc') ? $result : -$result; //Send final sort direction to usort
+		}
+
+		usort($this->data, 'usort_reorder');
 
 		/***********************************************************************
 		 * ---------------------------------------------------------------------
