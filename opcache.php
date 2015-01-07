a506cf4a926edf0202c7181e3017dd818e75a836
Daisuke Takahashi <daisuke@extendwings.com>
2015-01-07 22:00:20 +0900

    Enclose graph widget

--- opcache.php
+++ opcache.php
@@ -285,16 +285,7 @@ class OPcache_dashboard {
 	}
 
 	function render_widget_graph() {
-		?>
-			<form id="graph_ctrl">
-				<label><input type="radio" name="dataset" value="memory" checked><?php esc_html_e('Memory', 'opcache'); ?></label>
-				<label><input type="radio" name="dataset" value="keys"><?php esc_html_e('Keys', 'opcache'); ?></label>
-				<label><input type="radio" name="dataset" value="hits"><?php esc_html_e('Hits', 'opcache'); ?></label>
-			</form>
-			<div id="graph">
-				<div id="stats"></div>
-			</div>
-		<?php
+		$this->load_view('widgets/graph.php');
 	}
 
 	function render_admin_status_page() {
