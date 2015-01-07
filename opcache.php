64a51977bb889badfa032b346d9354423476b25a
Daisuke Takahashi <daisuke@extendwings.com>
2015-01-07 21:55:32 +0900

    Enclose version info widget

--- opcache.php
+++ opcache.php
@@ -273,18 +273,7 @@ class OPcache_dashboard {
 	}
 
 	function render_widget_version_info() {
-		$config = $this->data['config'];
-		$stats = $this->data['status']['opcache_statistics'];
-		$mem_stats = $this->data['status']['memory_usage'];
-		?>
-			<p id="hits"><?php printf('Hits: %s%%', $this->number_format( $stats['opcache_hit_rate'], 2) ); ?>
-			<p id="memory"><?php printf(
-					'Memory: %1$s of %2$s',
-					$this->size( $mem_stats['used_memory'] + $mem_stats['wasted_memory'] ),
-					$this->size( $config['directives']['opcache.memory_consumption'] )
-				); ?>
-			<p id="keys"><?php printf('Keys: %1$s of %2$s', $stats['num_cached_keys'], $stats['max_cached_keys'] ); ?>
-		<?php
+		$this->load_view('widgets/version-info.php', $this->data );
 	}
 
 	function render_widget_ctrl() {
